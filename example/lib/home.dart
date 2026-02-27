604ef757342e571b908b815d53ebb6a94c6bf1e8
muzammileusophtNewOct <pc.21695.muzammil@gmail.com>
2026-02-27 15:49:58 +0500

    delete unused assets

--- example/lib/home.dart
+++ example/lib/home.dart
@@ -1,14 +1,5 @@
-import 'package:arcore_flutter_plus_example/screens/augmented_faces.dart';
-import 'package:arcore_flutter_plus_example/screens/augmented_images.dart';
-import 'package:arcore_flutter_plus_example/screens/image_object.dart';
-import 'package:arcore_flutter_plus_example/screens/matri_3d.dart';
-import 'package:arcore_flutter_plus_example/screens/multiple_augmented_images.dart';
 import 'package:flutter/material.dart';
 import 'screens/hello_world.dart';
-import 'screens/custom_object.dart';
-import 'screens/runtime_materials.dart';
-import 'screens/texture_and_rotation.dart';
-import 'screens/assets_object.dart';
 import 'screens/auto_detect_plane.dart';
 import 'screens/remote_object.dart';
 
@@ -17,7 +8,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ArCore Demo'),
+        title: const Text('ArCore Plus Demo'),
       ),
       body: ListView(
         children: <Widget>[
@@ -26,84 +17,21 @@ class HomeScreen extends StatelessWidget {
               Navigator.of(context)
                   .push(MaterialPageRoute(builder: (context) => HelloWorld()));
             },
-            title: Text("Hello World"),
-          ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => ImageObjectScreen()));
-            },
-            title: Text("Image"),
-          ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => AugmentedPage()));
-            },
-            title: Text("AugmentedPage"),
-          ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => MultipleAugmentedImagesPage()));
-            },
-            title: Text("Multiple augmented images"),
-          ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => CustomObject()));
-            },
-            title: Text("Custom Anchored Object with onTap"),
-          ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => RuntimeMaterials()));
-            },
-            title: Text("Change Materials Property in runtime"),
-          ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => ObjectWithTextureAndRotation()));
-            },
-            title: Text("Custom object with texture and rotation listener "),
+            title: Text("Hello World (Shapes)"),
           ),
           ListTile(
             onTap: () {
               Navigator.of(context).push(
                   MaterialPageRoute(builder: (context) => AutoDetectPlane()));
             },
-            title: Text("Plane detect handler"),
-          ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => Matrix3DRenderingPage()));
-            },
-            title: Text("3D Matrix"),
-          ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => AssetsObject()));
-            },
-            title: Text("Custom sfb object"),
+            title: Text("Plane Detection"),
           ),
           ListTile(
             onTap: () {
               Navigator.of(context).push(
                   MaterialPageRoute(builder: (context) => RemoteObject()));
             },
-            title: Text("Remote object"),
-          ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => AugmentedFacesScreen()));
-            },
-            title: Text("Augmented Faces"),
+            title: Text("Remote Object (GLTF/GLB)"),
           ),
         ],
       ),
